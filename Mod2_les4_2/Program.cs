758c35f5fe9f75347ecb2cb9dfedb947f06e8bcf
Szymon Chyc <erressrsch@gmail.com>
2023-12-22 21:15:43 +0100

    Add project files.

---
+++ Mod2_les4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace modul2_les4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char g = 'g';
+            char h = 'h';
+            char i = 'i';
+
+            char[] letters = [g, h, i];
+            foreach (char letter in letters.Reverse())
+            {
+                Console.WriteLine(letters);
+            }
+
+        }
+    }
+}
